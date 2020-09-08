fd919de8ef28f9bef2123b71f974a785dcf0f5a1
Tom <64475286+TomSwallaxz@users.noreply.github.com>
2020-09-08 15:12:59 +0300

    indexJS

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,41 @@
-// 
+function calcAdd() {
+  txtBox1 = Number.getElementById("numberOne").value;
+  txtBox2 = Number.getElementById("numberTwo").value;
+
+  sum = txtBox1 + txtBox2;
+  Number.getElementById("result").value += sum;
+}
+
+function calcAdd() {
+  txtBox1 = Number.getElementById("numberOne").value;
+  txtBox2 = Number.getElementById("numberTwo").value;
+
+  sum = txtBox1 + txtBox2;
+  Document.getElementById("result").innerHTML = sum;
+}
+
+function calcSub() {
+  let firstInput = document.getElementById("numberOne").value;
+  let secondInput = document.getElementById("numberTwo").value;
+
+  document.getElementById("result").innerHTML = firstInput - secondInput;
+}
+
+function calcMul() {
+  // Get <input> elements
+  let firstInput = document.getElementById("numberOne");
+  let secondInput = document.getElementById("numberTwo");
+
+  // Parse the text entered in each into numbers
+  var num1 = Number(firstInput.value);
+  var num2 = Number(secondInput.value);
+
+  // Find their sum
+  var sum = num1 + num2;
+
+  // Get the output element
+  var resultElement = document.getElementById("result");
+
+  // Display the sum in the output element
+  resultElement.innerHTML = sum;
+}
